a701056065fa8bf370a167881d56b16ec20e518d
aliya.tang <aliya.w.tang@gmail.com>
2023-10-29 08:27:47 -0700

    site fix and vanta stuff

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/aliyaBlog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1698586188';
+const cacheName = 'chirpy-1698593254';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
